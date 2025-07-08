f4c46b465a3a7031fe55e182dcb85355ba6c0c60
oki3ed <kokosik3215@gmail.com>
2025-07-08 05:01:25 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+// So that there is no endless creation of code (build)
+#include <Geode/Geode.hpp>
+/*
+$on_mod(Loaded) {
+    makeSexWithGays(2);
+}
+*/
+// I forgot to delete this shit
+// While doing gay sex
+/*  🧱
+  🧱🧱🧱
+🧱🧱🧱🧱🧱
+🧱🧱🚪🧱🧱
+🧱🧱🧱🧱🧱
+I'm play minecraft is my home
+*/
